01ffd226a69250419aa02e2b9763e3abd41f82fb
p-sam <p-sam@d3vs.net>
2016-09-24 12:11:25 +0000

    First Iteration

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # php-set
 A PHP Set class limited to scalar using hashmaps
+
+## Example
+
+```php
+use SP\Set\Set;
+
+$set = Set::fromArray([2,3]);
+
+foreach($set as $k=>$v) {
+	var_dump($k);
+	var_dump($v);
+} 
+
+var_dump($set->toArray());
+
+$set->add(5);
+var_dump($set->toArray());
+
+$set->add(3);
+var_dump($set->toArray());
+
+$set->remove(3);
+var_dump($set->toArray());
+
+$set->remove(1);
+var_dump($set->toArray());
+
+var_dump($set->has(1));
+
+var_dump($set->has(2));
+```
+
+## Expected Output
+
+```php
+int(0)
+int(2)
+int(1)
+int(3)
+array(2) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(3)
+}
+array(3) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(3)
+  [2]=>
+  int(5)
+}
+array(3) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(3)
+  [2]=>
+  int(5)
+}
+array(2) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(5)
+}
+array(2) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(5)
+}
+bool(false)
+bool(true)
+```
---
+++ Set.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SP\Set;
+
+class Set implements \IteratorAggregate {
+	private $hashmap;
+	private $iterator;
+	
+	private static function assertScalar($scalar) {
+		if(!is_scalar($scalar)) {
+			throw new \InvalidArgumentException('Only scalar can be added to this Set (due to PHP limitations)');
+		}
+	}
+	
+	public static function fromArray(array $array) {
+		$set = new Set();
+		foreach($array as $v) {
+			$set->add($v);
+		}
+		return $set;
+	}
+	
+	public function __construct() {
+		$this->hashmap = [];
+		$this->iterator = null;
+	}
+	
+	public function getIterator() {
+		if(is_null($this->iterator)) {
+			$this->iterator = new Iterator($this->hashmap);
+		}
+		return $this->iterator;
+	}
+	
+	public function toArray() {
+		return array_keys($this->hashmap);
+	}
+	
+	public function has($scalar) {
+		self::assertScalar($scalar);
+		return isset($this->hashmap[$scalar]);
+	}
+	
+	public function add($scalar) {
+		if(!$this->has($scalar)) {
+			$this->hashmap[$scalar] = 1;
+		}
+	}
+	
+	public function remove($scalar) {
+		if($this->has($scalar)) {
+			unset($this->hashmap[$scalar]);
+		}
+	}
+	
+}
